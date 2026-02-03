e6c46cdfe92453204aecd5765b0baec10a351729
AhanaSaraff <ahanasaraff@gmail.com>
2026-02-03 09:42:52 +0530

    Exception Handling

--- say.py
+++ say.py
@@ -1,5 +1,11 @@
 import cowsay
 import sys
 
+# if len(sys.argv) == 2:
+#     cowsay.trex("hello, "+ sys.argv[1])
+
+from sayings import hello
+
 if len(sys.argv) == 2:
-    cowsay.trex("hello, "+ sys.argv[1])
+    hello(sys.argv[1])
+
